b299fa0e0351a63d6693be3d8e1c03c7f01765fb
Chris Cannam <cannam@all-day-breakfast.com>
2020-12-02 08:50:04 +0000

    Add logo!

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -2,12 +2,12 @@
 #
 # To be run from a Meson run target:
 #
-# $ meson compile doc
+# $ meson compile -C build doc
 
 set -e
 
 cd "${MESON_SOURCE_ROOT}"
 mkdir -p doc
-cp scripts/screen.css doc/
+cp resources/screen.css resources/bisquay.svg doc/
 ./sml-buildscripts/mlb-expand test-sml.mlb > .docfiles
-smldoc --nowarn --windowtitle=Bisquay --header=Bisquay --charset=UTF-8 --stylesheet=screen.css -d doc -a .docfiles 
+smldoc --nowarn --overview=resources/doc-overview.html --windowtitle=Bisquay --header=Bisquay --charset=UTF-8 --stylesheet=screen.css -d doc -a .docfiles 
