2a0e58f4f284ab889adb53d99ac656d6a3e0f9dc
Chris Cannam <cannam@all-day-breakfast.com>
2025-02-26 18:57:00 +0000

    Simplify build for ffi a little bit

---
+++ ffi-bundle.cpp
@@ -0,0 +1,10 @@
+
+#include "bsq-bq/ffi/impl-bundle.cpp"
+
+extern "C" {
+#include "sml-stringinterpolate/number-string.c"
+#include "bsq-matrix/ffi/copyvec.c"
+#include "bsq-rrloop/ffi/rr-support.c"
+#include "bsq-signalbits/ffi/window-post.c"
+#include "bsq-signalbits/ffi/distance.c"
+}
